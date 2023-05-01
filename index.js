e77fc2590531fab4586aff4cbc1d83fe5d08a0cf
Mai <garcia.ton94@gmail.com>
2023-05-01 13:20:54 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return receivesAFunction;
+}
+
+function returnsAnAnonymousFunction () {
+    return function() {
+        return ''};
+};
